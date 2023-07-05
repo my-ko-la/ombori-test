a3ff765f14bb99628eb7d57706c156c761e1a389
Mykola Brechko <brechko.mv@gmail.com>
2023-07-05 19:04:12 +0200

    change color styling

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className='flex flex-row items-center justify-center min-w-full top-0 h-14 py-2 bg-gray-100 text-gray-800 border-b-slate-500 fixed border-b z-50'>
+      <header className='flex flex-row items-center justify-center min-w-full top-0 h-14 py-2 bg-gray-100 text-slate-900  border-b-[#c4d9bc] fixed border-b z-50'>
         <h1 className='text-2xl sm:text-4xl font-bold'>Users</h1>
       </header>
     </>
--- src/components/users/User.tsx
+++ src/components/users/User.tsx
@@ -12,20 +12,20 @@ export const UserComponent: React.FC<User> = ({
   return (
     <div
       key={id}
-      className='flex flex-row bg-slate-200 shadow-lg items-center px-4 w-72 h-24 rounded-lg
+      className='flex flex-row bg-[#94b584a9] shadow-lg items-center px-4 w-72 h-24 rounded-lg
       sm:w-96 sm:h-32 gap-x-3 sm:gap-x-6
       md:gap-x-2 md:w-80 md:h-28'
     >
       <img
         alt="User's avatar"
         src={avatar}
-        className='sm:w-20 sm:h-20 w-14 h-14 rounded-full shadow-md'
+        className='sm:w-20 sm:h-20 w-14 h-14 rounded-full shadow-sm shadow-green-800'
       />
-      <div className='flex font-mono flex-col items-center justify-between p-2'>
-        <p className='font-semibold text-sm sm:text-md'>
+      <div className='flex font-mono flex-col items-center min-w-[198px] justify-between p-2'>
+        <p className='font-semibold text-sm text-slate-900 sm:text-md'>
           {first_name} {last_name}
         </p>
-        <p className='text-xs italic sm:text-sm'>{email}</p>
+        <p className='text-xs opacity-40 overflow-hidden'>{email}</p>
       </div>
     </div>
   );
